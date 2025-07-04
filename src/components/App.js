71ca2649d9aa4397fa64a88be7e1f942d6b286e6
Sumitsoni21 <128276751+Sumitsoni21@users.noreply.github.com>
2025-07-05 01:34:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,38 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [task, setTask] = useState('');
+ const [tasks, setTasks] = useState([]);
+ 
+ const handleAddTask = (e) => {
+   setTasks((prevTask) => [...prevTask, task]);
+   setTask("");
+ }
+ 
+ const handleChange = (e) => {
+   setTask(e.target.value);
+ }
+ 
+ const handleDelete = (i) => {
+   setTasks(tasks.filter((_,index) => i !== index))
+ }
+  
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+      <div>
+        
+       <h2>To-Do List</h2>
+       
+       <input type="text" value={task} onChange={handleChange}/>
+       <button onClick={handleAddTask}>Add Todo</button>
+       
+       {tasks && <div>   {tasks.map((task,index) => <div>
+         <p>{task}</p>
+         <button onClick={()=>handleDelete(index)}>Delete</button>
+       </div>)}
+       
+       </div>}
+     
+      </div>
   )
 }
 
