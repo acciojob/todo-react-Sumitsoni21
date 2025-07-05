0402f9c37ac134100099b6ee3946906c9e279ed6
Sumitsoni21 <128276751+Sumitsoni21@users.noreply.github.com>
2025-07-05 15:45:38 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,1 +1,17 @@
 
+
+import react, { useState } from "react";
+
+function Todo = ({tasks, handleDelete}) => {
+
+   <ul className="todoList" >
+     {tasks.map((task) => (
+       <li>
+         <p>{task}</p>
+         <button className="btn"  onClick={()=>handleDelete(index)}>Delete</button>
+       </li>
+  )}
+  </ul>
+}
+
+export default Todo;
